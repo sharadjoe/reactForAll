e8ac5338739bec04f66fd9d5ee2f138449279b14
Sharad Joe <sharadjoe48@gmail.com>
2023-04-18 20:10:06 +0530

    level 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AppContainer from "./AppContainer";
+import Header from "./Header";
+// import "./App.css";
+
+const formFields = [
+  { id: 1, label: "First Name" },
+  { id: 2, label: "Last Name" },
+  { id: 3, label: "Email" },
+  { id: 4, label: "Date" }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <div className="flex h-screen bg-gray-100 items-center justify-center w-full">
+        <div className="p-4 mx-auto bg-white shadow-lg rounded-xl ">
+          <Header title={"Hello World"} />
+
+          {formFields.map((field) => (
+            <React.Fragment key={field.id}>
+              <label htmlFor={field.label}>{field.label}</label>
+              <input
+                type={field.label === "Date" ? "date" : "text"}
+                placeholder={
+                  field.label === "Data" ? "dd/mm/yyyy" : field.label
+                }
+                className="border-2 border-gray-200 rounded-lg p-2 m-2 w-full"
+                id={field.label}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    </AppContainer>
   );
 }
 
